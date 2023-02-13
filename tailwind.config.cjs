a8d55ccb414623e2af55e8e50b5a14f1e8343726
Mussie Teshome <Mussieteshomeg@gmail.com>
2023-02-13 12:44:35 +0300

    load the configuration

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -12,8 +12,10 @@ module.exports = {
       primary: "#020f24",
       white: "#FFFFFF",
       secondary: "#64FFDA",
+      trial: "#0c1421"
     },
   },
   plugins: [],
 };
 // #031229, #0c1421, #020f24, #64FFDA
+// 21, 26, 30
